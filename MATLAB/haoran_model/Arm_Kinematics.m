a67d64178eb1f13115b25538f43ade997b41c1eb
Haoran Yu <Haoran Yu>
2016-03-09 18:48:28 -0800

    build a class to store link FK

--- MATLAB/haoran_model/Arm_Kinematics.m
+++ MATLAB/haoran_model/Arm_Kinematics.m
@@ -15,9 +15,24 @@ classdef Arm_Kinematics
             link_input = urdf_input.links;
             joint_input = urdf_input.joints;
             Arm_Kinematics_out(length(link_input)) = Arm_Kinematics;
+            Arm_Kinematics_out(1).Tran_matrix = eye(4);
             for i = 1:length(link_input)
                 Arm_Kinematics_out(i).name = link_input{i}.name;
-                Arm_Kinematics_out(i).Tran_matrix = eye(4);
+                if i<length(link_input) && i>1
+                d = joint_input{i-1}.origin.xyz;
+                euler = joint_input{i-1}.origin.rpy;
+                if i>9
+                    joint_angle = 0;
+                else
+                    joint_angle = q(i);
+                end
+                rot_matrix = RotationMatrix_rad(joint_angle,[0;0;1])*...
+                             RotationMatrix_rad(euler(1),[1;0;0])*...
+                             RotationMatrix_rad(euler(2),[0;1;0])*...
+                             RotationMatrix_rad(euler(3),[0;0;1]);
+                T = [rot_matrix d'; 0 0 0 1];
+                Arm_Kinematics_out(i).Tran_matrix = Arm_Kinematics_out(i-1).Tran_matrix * T;
+                end
                 Arm_Kinematics_out(i).stl_name = strcat(link_input{i}.name,'.stl');
                 if isfield(link_input{i},'inertial')
                 Arm_Kinematics_out(i).mass = link_input{i}.inertial.mass;
--- MATLAB/haoran_model/test.m
+++ MATLAB/haoran_model/test.m
@@ -1,3 +1,12 @@
 URDF_file= 'V1_Arm_URDF.URDF';
 q=[0,0,0,0,0,0,0,0,0];
 Arm_Kinematics1 = Arm_Kinematics(URDF_file,q);
+figure(1)
+hold on 
+view(3)
+axis equal
+for i = 1:12
+fv=stlread(Arm_Kinematics1(i).stl_name);
+patch(fv,'FaceColor',       rand(3,1), ...
+         'EdgeColor',       'none');
+end
