efb24e303d6146ed258a274eb08025b60fa788b4
Lee Magnusson <lee@motive.ai>
2016-05-03 14:25:22 -0700

    Some little optimizations on braking analysis

--- MATLAB/lee_model/analysis/braking.m
+++ MATLAB/lee_model/analysis/braking.m
@@ -16,7 +16,7 @@ end
 
 
 % Get a bunch of q positions
-q_list = q0%GetPositionArray(r,u);%q0;
+q_list = GetPositionArray(r,u);%q0;
 
 % Set tool position to end
 r.tool(3,4) = .28;  % m
@@ -30,21 +30,24 @@ sat = @(x,lim_min,lim_max) min(max(x,lim_min),lim_max);
 t_p = zeros(size(q_list));
 t_g = zeros(size(q_list));
 Tm_dyn_p = zeros(size(q_list));
-for i = 1:size(q_list,1)
+parfor i = 1:size(q_list,1)
     if (mod(i,1000)==0)
         disp(i);
     end
     q = q_list(i,:);
-    t_p(i,:) = JointImpulse(r,q,v_i);
-    t_g(i,:) = r.gravload(q);
+    t_p_tmp = JointImpulse(r,q,v_i)';
+    t_g_tmp = r.gravload(q);
 
-     qd = zeros(size(q));
-     [t,qs,qds] = r.fdyn(t_stop,@(r,t,q,qd) t_g(i,:)+t_p(i,:)/t_stop, q, qd);
-     xs = r.fkine(q) - r.fkine(qs(end,:));
-     xds = r.jacob0(q)*qds(end,:)';
+    qd = zeros(size(q));
+    [t,qs,qds] = r.fdyn(t_stop,@(r,t,q,qd) t_g_tmp+t_p_tmp/t_stop, q, qd);
+    xs = r.fkine(q) - r.fkine(qs(end,:));
+    xds = r.jacob0(q)*qds(end,:)';
+ 
+    Tm_dyn = sat(repmat(b*100^2,size(qds,1),1).*qds,-repmat(Tmax*100,size(qds,1),1),repmat(Tmax*100,size(qds,1),1));
+    Tm_dyn_p(i,:) = trapz(t,Tm_dyn);  % motor impulse due to braking
 
-     Tm_dyn = sat(repmat(b*100^2,size(qds,1),1).*qds,-repmat(Tmax*100,size(qds,1),1),repmat(Tmax*100,size(qds,1),1));
-     Tm_dyn_p(i,:) = trapz(t,Tm_dyn);  % motor impulse due to braking
+    t_p(i,:) = t_p_tmp;
+    t_g(i,:) = t_g_tmp;
 end
 %%
 [t_p_max,i_impulse_max] = max(abs(t_p),[],1);
